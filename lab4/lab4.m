b596665b0f6189330f1d882fdc96591f91fe741b
Arthur Vernet <arthur.vernet@epfl.ch>
2018-05-07 12:46:45 +0100

    fix

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -1,24 +1,23 @@
-% %% R1.b)
-% load("energy_train.mat");
-% N = 96;
-% a = longtermpred(x_train, N);
-% figure
-% title('Graph of Sine and Cosine Between -2\pi and 2\pi')
-% plot(x_train(N+1:end));
-% x_pred = a*x_train(N+1:end);
-% figure
-% plot(x_pred);
-% r = x_train(N+1:end)-x_pred;
-% figure
-% plot(r)
-% 
-% %% R1.c)
-% load("energy_train.mat");
-% N = 96;
-% a = longtermpred(x_train, N);
-% x_pred = a*x_train(N+1:end);
-% r = x_train(N+1:end)-x_pred;
-% E = sum(r)^2;
+%% R1.b)
+load("energy_train.mat");
+N = 96;
+a = longtermpred(x_train, N);
+figure
+plot(x_train(N+1:end));
+x_pred = a*x_train(1:end-N);
+figure
+plot(x_pred);
+r = x_train(N+1:end)-x_pred;
+figure
+plot(r)
+
+%% R1.c)
+load("energy_train.mat");
+N = 96;
+a = longtermpred(x_train, N);
+x_pred = a*x_train(N+1:end);
+r = x_train(N+1:end)-x_pred;
+E = sum(r)^2;
 
 %% R1.e)
 load("energy_train.mat");
@@ -28,11 +27,11 @@ figure
 plot(x_train(P+1:end));
 x_pred = zeros(size(x_train, 1) - P, 1);
 for i = 1:size(x_pred, 1)
-    sum = 0;
+    s = 0;
     for k = 1:P
-        sum = sum + a(k)*x_train(P+i-k);
+        s = s + a(k)*x_train(P+i-k);
     end
-    x_pred(i) = sum;
+    x_pred(i) = s;
 end
 figure
 plot(x_pred);
@@ -57,3 +56,4 @@ E = sum(r)^2;
 
 %% R2.b)
 load("energy_test.mat");
+plot(x_test);
