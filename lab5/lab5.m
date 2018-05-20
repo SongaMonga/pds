0b4391adac2b16e7bf6c77a17769139e86b72db0
Arthur Vernet <arthur.vernet@epfl.ch>
2018-05-20 16:57:45 +0100

    part 1 done

---
+++ lab5/lab5.m
@@ -0,0 +1,54 @@
+%% R1.b)
+clear all; close all
+load("sar_image.mat");
+colormap cool %apply a different color mapping to better distinguish ice from water
+imagesc(I);
+
+%% R1.c)
+clear all; close all
+load("sar_image.mat");
+ice = imcrop(I, [760 2453 949 188]); %big lower right chunk of ice
+water = imcrop(I, [1 1 629 1234]); %big upper left chunk of water
+%compute the different Maximum Likelihood Estimators for both ice and water
+p_ice_exp = mle(ice(:), 'distribution', 'Exponential');
+p_ice_ray = mle(ice(:), 'distribution', 'Rayleigh');
+p_ice_nor = mle(ice(:), 'distribution', 'Normal');
+p_water_exp = mle(water(:), 'distribution', 'Exponential');
+p_water_ray = mle(water(:), 'distribution', 'Rayleigh');
+p_water_nor = mle(water(:), 'distribution', 'Normal');
+
+%% R1.d)
+clear all; close all
+load("sar_image.mat");
+ice = imcrop(I, [760 2453 949 188]);
+water = imcrop(I, [1 1 629 1234]);
+p_ice_exp = mle(ice(:), 'distribution', 'Exponential');
+p_ice_ray = mle(ice(:), 'distribution', 'Rayleigh');
+p_ice_nor = mle(ice(:), 'distribution', 'Normal');
+p_water_exp = mle(water(:), 'distribution', 'Exponential');
+p_water_ray = mle(water(:), 'distribution', 'Rayleigh');
+p_water_nor = mle(water(:), 'distribution', 'Normal');
+
+figure('Name', 'Ice'); histogram(ice(:), 'Normalization', 'probability') %normalized histogram of the ice values
+ice_exp = exppdf(ice(:), p_ice_exp); %compute the pdf with exponential distribution
+figure('Name', 'Ice - Exponential'); scatter(ice(:), ice_exp, 'filled')
+ice_ray = raylpdf(ice(:), p_ice_ray); %compute the pdf with Rayleigh distribution
+figure('Name', 'Ice - Rayleigh'); scatter(ice(:), ice_ray, 'filled')
+ice_nor = normpdf(ice(:), p_ice_nor(1), p_ice_nor(2)); %compute the pdf with normal distribution
+figure('Name', 'Ice - Normal'); scatter(ice(:), ice_nor, 'filled')
+
+figure('Name', 'Water'); histogram(water(:), 'Normalization', 'probability') %normalized histogram of the water values
+water_exp = exppdf(water(:), p_water_exp); %compute the pdf with exponential distribution
+figure('Name', 'Water - Exponential'); scatter(water(:), water_exp, 'filled')
+water_ray = raylpdf(water(:), p_water_ray); %compute the pdf with Rayleigh distribution
+figure('Name', 'Water - Rayleigh'); scatter(water(:), water_ray, 'filled')
+water_nor = normpdf(water(:), p_water_nor(1), p_water_nor(2)); %compute the pdf with normal distribution
+figure('Name', 'Water - Normal'); scatter(water(:), water_nor, 'filled')
+
+%% R2.a)
+
+%% R2.b)
+
+%% R2.c)
+
+%% R2.d)
