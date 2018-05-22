ba4b814d5cdaa73e823eb9c417d6dd0513e6a3a2
Arthur Vernet <arthur.vernet@epfl.ch>
2018-05-22 22:57:53 +0100

    fix hopefulyyyyy

--- lab5/lab5.m
+++ lab5/lab5.m
@@ -1,70 +1,68 @@
-% %% R1.b)
-% clear all; close all
-% load("sar_image.mat");
-% figure; colormap hsv %apply a different color mapping to better distinguish ice from water
-% imagesc(I); truesize 
-% 
-% %% R1.c)
-% clear all; close all
-% load("sar_image.mat");
-% ice = imcrop(I, [760 2453 949 188]); %big lower right chunk of ice
-% water = imcrop(I, [1 1 629 1234]); %big upper left chunk of water
-% %compute the different Maximum Likelihood Estimators for both ice and water
-% p_ice_exp = mle(ice(:), 'distribution', 'Exponential');
-% p_ice_ray = mle(ice(:), 'distribution', 'Rayleigh');
-% p_ice_nor = mle(ice(:), 'distribution', 'Normal');
-% p_water_exp = mle(water(:), 'distribution', 'Exponential');
-% p_water_ray = mle(water(:), 'distribution', 'Rayleigh');
-% p_water_nor = mle(water(:), 'distribution', 'Normal');
-% 
-% %% R1.d)
-% clear all; close all
-% load("sar_image.mat");
-% ice = imcrop(I, [760 2453 949 188]);
-% water = imcrop(I, [1 1 629 1234]);
-% p_ice_exp = mle(ice(:), 'distribution', 'Exponential');
-% p_ice_ray = mle(ice(:), 'distribution', 'Rayleigh');
-% p_ice_nor = mle(ice(:), 'distribution', 'Normal');
-% p_water_exp = mle(water(:), 'distribution', 'Exponential');
-% p_water_ray = mle(water(:), 'distribution', 'Rayleigh');
-% p_water_nor = mle(water(:), 'distribution', 'Normal');
-% 
-% figure('Name', 'Ice'); histogram(ice(:), 'Normalization', 'probability') %normalized histogram of the ice values
-% ice_exp = exppdf(ice(:), p_ice_exp); %compute the pdf with exponential distribution
-% figure('Name', 'Ice - Exponential'); scatter(ice(:), ice_exp, 'filled')
-% ice_ray = raylpdf(ice(:), p_ice_ray); %compute the pdf with Rayleigh distribution
-% figure('Name', 'Ice - Rayleigh'); scatter(ice(:), ice_ray, 'filled')
-% ice_nor = normpdf(ice(:), p_ice_nor(1), p_ice_nor(2)); %compute the pdf with normal distribution
-% figure('Name', 'Ice - Normal'); scatter(ice(:), ice_nor, 'filled')
-% 
-% figure('Name', 'Water'); histogram(water(:), 'Normalization', 'probability') %normalized histogram of the water values
-% water_exp = exppdf(water(:), p_water_exp); %compute the pdf with exponential distribution
-% figure('Name', 'Water - Exponential'); scatter(water(:), water_exp, 'filled')
-% water_ray = raylpdf(water(:), p_water_ray); %compute the pdf with Rayleigh distribution
-% figure('Name', 'Water - Rayleigh'); scatter(water(:), water_ray, 'filled')
-% water_nor = normpdf(water(:), p_water_nor(1), p_water_nor(2)); %compute the pdf with normal distribution
-% figure('Name', 'Water - Normal'); scatter(water(:), water_nor, 'filled')
-% 
-% %% R2.a)
-% load("sar_image.mat");
-% ice = imcrop(I, [760 2453 949 188]);
-% water = imcrop(I, [1 1 629 1234]);
-% p_ice_nor = mle(ice(:), 'distribution', 'Normal');
-% p_water_nor = mle(water(:), 'distribution', 'Normal');
-% ice_nor = normpdf(ice(:), p_ice_nor(1), p_ice_nor(2));
-% water_nor = normpdf(water(:), p_water_nor(1), p_water_nor(2));
-% 
-% I_A = zeros(size(I)); %store the image segmented by regions pixel by pixel
-% for x = 1:size(I, 1)
-%     for y = 1:size(I, 2)
-%         prob1 = ice_nor(I(x,y));
-%         prob2 = water_nor(I(x,y));
-%         I_A(x,y) = prob1 < prob2;
-%     end
-% end
-% 
-% figure; colormap hsv
-% imcontour(I_A, 1) %draws the contour of the regions
+%% R1.b)
+clear all; close all
+load("sar_image.mat");
+figure; colormap hsv %apply a different color mapping to better distinguish ice from water
+imagesc(I); truesize 
+
+%% R1.c)
+clear all; close all
+load("sar_image.mat");
+ice = imcrop(I, [760 2453 949 188]); %big lower right chunk of ice
+water = imcrop(I, [1 1 629 1234]); %big upper left chunk of water
+%compute the different Maximum Likelihood Estimators for both ice and water
+p_ice_exp = mle(ice(:), 'distribution', 'Exponential');
+p_ice_ray = mle(ice(:), 'distribution', 'Rayleigh');
+p_ice_nor = mle(ice(:), 'distribution', 'Normal');
+p_water_exp = mle(water(:), 'distribution', 'Exponential');
+p_water_ray = mle(water(:), 'distribution', 'Rayleigh');
+p_water_nor = mle(water(:), 'distribution', 'Normal');
+
+%% R1.d)
+clear all; close all
+load("sar_image.mat");
+ice = imcrop(I, [760 2453 949 188]);
+water = imcrop(I, [1 1 629 1234]);
+p_ice_exp = mle(ice(:), 'distribution', 'Exponential');
+p_ice_ray = mle(ice(:), 'distribution', 'Rayleigh');
+p_ice_nor = mle(ice(:), 'distribution', 'Normal');
+p_water_exp = mle(water(:), 'distribution', 'Exponential');
+p_water_ray = mle(water(:), 'distribution', 'Rayleigh');
+p_water_nor = mle(water(:), 'distribution', 'Normal');
+
+figure('Name', 'Ice'); histogram(ice(:), 'Normalization', 'probability') %normalized histogram of the ice values
+ice_exp = exppdf(ice(:), p_ice_exp); %compute the pdf with exponential distribution
+figure('Name', 'Ice - Exponential'); scatter(ice(:), ice_exp, 'filled')
+ice_ray = raylpdf(ice(:), p_ice_ray); %compute the pdf with Rayleigh distribution
+figure('Name', 'Ice - Rayleigh'); scatter(ice(:), ice_ray, 'filled')
+ice_nor = normpdf(ice(:), p_ice_nor(1), p_ice_nor(2)); %compute the pdf with normal distribution
+figure('Name', 'Ice - Normal'); scatter(ice(:), ice_nor, 'filled')
+
+figure('Name', 'Water'); histogram(water(:), 'Normalization', 'probability') %normalized histogram of the water values
+water_exp = exppdf(water(:), p_water_exp); %compute the pdf with exponential distribution
+figure('Name', 'Water - Exponential'); scatter(water(:), water_exp, 'filled')
+water_ray = raylpdf(water(:), p_water_ray); %compute the pdf with Rayleigh distribution
+figure('Name', 'Water - Rayleigh'); scatter(water(:), water_ray, 'filled')
+water_nor = normpdf(water(:), p_water_nor(1), p_water_nor(2)); %compute the pdf with normal distribution
+figure('Name', 'Water - Normal'); scatter(water(:), water_nor, 'filled')
+
+%% R2.a)
+load("sar_image.mat");
+ice = imcrop(I, [760 2453 949 188]);
+water = imcrop(I, [1 1 629 1234]);
+p_ice_nor = mle(ice(:), 'distribution', 'Normal');
+p_water_nor = mle(water(:), 'distribution', 'Normal');
+
+I_A = zeros(size(I)); %store the image segmented by regions pixel by pixel
+for x = 1:size(I, 1)
+    for y = 1:size(I, 2)
+        prob1 = normpdf(I(x,y), p_ice_nor(1), p_ice_nor(2));
+        prob2 = normpdf(I(x,y), p_water_nor(1), p_water_nor(2));
+        I_A(x,y) = prob1 < prob2;
+    end
+end
+
+figure; colormap hsv
+imcontour(I_A, 1) %draws the contour of the regions
 
 %% R2.b)
 load("sar_image.mat");
@@ -72,8 +70,6 @@ ice = imcrop(I, [760 2453 949 188]);
 water = imcrop(I, [1 1 629 1234]);
 p_ice_nor = mle(ice(:), 'distribution', 'Normal');
 p_water_nor = mle(water(:), 'distribution', 'Normal');
-ice_nor = normpdf(ice(:), p_ice_nor(1), p_ice_nor(2));
-water_nor = normpdf(water(:), p_water_nor(1), p_water_nor(2));
 
 I_B = zeros(size(I)); %store the image segmented by regions by 9x9 patch of pixel
 for x = 1:size(I, 1)
@@ -88,8 +84,8 @@ for x = 1:size(I, 1)
                 end
             end
         end
-        prob1 = ice_nor(val);
-        prob2 = water_nor(val);
+        prob1 = normpdf(val, p_ice_nor(1), p_ice_nor(2));
+        prob2 = normpdf(val, p_water_nor(1), p_water_nor(2));
         I_B(x,y) = mean(prob1 < prob2) > 0.5;
     end
 end
